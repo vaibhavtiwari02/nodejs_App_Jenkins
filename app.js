c22173dddc8e384164efec92e27bd21e9ffe0391
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-08-20 12:11:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 // Configure the HTTP server to respond with "Hello, World!" to all requests.
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
