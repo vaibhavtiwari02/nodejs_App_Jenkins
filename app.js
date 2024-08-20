f242dbd82be58cb808c6feac227032c6e6320484
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-08-20 11:37:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// Load the http module to create an HTTP server.
+const http = require('http');
+
+// Configure the HTTP server to respond with "Hello, World!" to all requests.
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000 and IP 127.0.0.1.
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
